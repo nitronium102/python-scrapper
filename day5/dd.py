dd299ffd36c72b4eb88bf339aae31f6bc5b42a82
nitronium102 <68282057+nitronium102@users.noreply.github.com>
2021-03-10 02:18:41 +0900

    Add files via upload

---
+++ day5/dd.py
@@ -0,0 +1,4 @@
+choice_num = int(input("#: "))
+if int(choice_num) >=0 and int(choice_num) < 265:
+  print("You choose",countries[int(choice_num)])
+  print("The currency code is",codes[int(choice_num)])
